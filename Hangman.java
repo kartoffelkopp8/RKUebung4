b1b97772260fc581859307baec26984537a77c29
MaximilianHausen <max.ha@directbox.com>
2024-05-29 03:29:35 +0200

    Telnet server improvements
    - Fixed input handling
      - ! as full word trigger
      - Retry until valid input
    - Better messages
      - Player counts during init
      - Better input prompts
    - Consistent formatting (Tabs)

--- Hangman.java
+++ Hangman.java
@@ -11,249 +11,249 @@ import java.util.Random;
  */
 public class Hangman {
 
-  /**
-   * Collection of words to be guessed.
-   */
-  private static final String[] WORDS = { "Feuerwehr", "Autobahn",
-      "Informatik", "Fussball", "Schreibtisch", "Butterbrot",
-      "Colaweizen", "Arbeitsamt", "Bundeskanzler", "Kartoffel",
-      "Gefrierpunkt", "Schichtenarchitektur", "Protokoll",
-      "Ausbreitungsverzoegerung", "Rechnerkommunikation", "Bergdienstag",
-      "Klausuraufgabe", "Medienzugriff", "Paketvermittlung",
-      "Warteschlange" };
+	/**
+	 * Collection of words to be guessed.
+	 */
+	private static final String[] WORDS = { "Feuerwehr", "Autobahn",
+			"Informatik", "Fussball", "Schreibtisch", "Butterbrot",
+			"Colaweizen", "Arbeitsamt", "Bundeskanzler", "Kartoffel",
+			"Gefrierpunkt", "Schichtenarchitektur", "Protokoll",
+			"Ausbreitungsverzoegerung", "Rechnerkommunikation", "Bergdienstag",
+			"Klausuraufgabe", "Medienzugriff", "Paketvermittlung",
+			"Warteschlange" };
 
-  /** ArrayList containing the correctly guessed letters. */
-  private ArrayList<Character> guesses;
+	/** ArrayList containing the correctly guessed letters. */
+	private ArrayList<Character> guesses;
 
-  /** ArrayList containing guessed letters which are not part of the word. */
-  private ArrayList<Character> misses;
+	/** ArrayList containing guessed letters which are not part of the word. */
+	private ArrayList<Character> misses;
 
-  /** The word to be guessed. */
-  private final String word;
+	/** The word to be guessed. */
+	private final String word;
 
-  /** Number of wrong guesses. */
-  private int numMissed;
+	/** Number of wrong guesses. */
+	private int numMissed;
 
-  /** Indicates if the solution has already been found. */
-  private boolean solved;
+	/** Indicates if the solution has already been found. */
+	private boolean solved;
 
-  /**
-   * Constructor, creates a new hangman game.
-   */
-  public Hangman() {
-    Random r = new Random();
-    word = WORDS[r.nextInt(WORDS.length)];
-    guesses = new ArrayList<Character>();
-    misses = new ArrayList<Character>();
-    numMissed = 1;
-    solved = false;
-  }
+	/**
+	 * Constructor, creates a new hangman game.
+	 */
+	public Hangman() {
+		Random r = new Random();
+		word = WORDS[r.nextInt(WORDS.length)];
+		guesses = new ArrayList<Character>();
+		misses = new ArrayList<Character>();
+		numMissed = 1;
+		solved = false;
+	}
 
-  /**
-   * Checks whether an entered letter is part of the word or not or has
-   * already been guessed.
-   * 
-   * @param c
-   *          The character to be checked.
-   * @return A short evaluating message.
-   */
-  public String checkChar(char c) {
-    String msg;
+	/**
+	 * Checks whether an entered letter is part of the word or not or has
+	 * already been guessed.
+	 * 
+	 * @param c
+	 *          The character to be checked.
+	 * @return A short evaluating message.
+	 */
+	public String checkChar(char c) {
+		String msg;
 
-    if (c < 'a' || c > 'z') {
-      msg = "Nur kleine Buchstaben von a-z sind erlaubt!\r\n";
-      ++numMissed;
-    } else if (guesses.contains(c) || misses.contains(c)) {
-      msg = "Dieser Buchstabe wurde bereits genannt... Besser aufpassen!\r\n";
-      ++numMissed;
-    } else if (word.toLowerCase().indexOf(c) != -1) {
-      msg = "Gut geraten! Dieser Buchstabe ist enthalten!\r\n";
-      guesses.add(c);
-    } else {
-      msg = "Leider nicht enthalten!\r\n";
-      ++numMissed;
-      misses.add(c);
-    }
+		if (c < 'a' || c > 'z') {
+			msg = "Nur kleine Buchstaben von a-z sind erlaubt!\r\n";
+			++numMissed;
+		} else if (guesses.contains(c) || misses.contains(c)) {
+			msg = "Dieser Buchstabe wurde bereits genannt... Besser aufpassen!\r\n";
+			++numMissed;
+		} else if (word.toLowerCase().indexOf(c) != -1) {
+			msg = "Gut geraten! Dieser Buchstabe ist enthalten!\r\n";
+			guesses.add(c);
+		} else {
+			msg = "Leider nicht enthalten!\r\n";
+			++numMissed;
+			misses.add(c);
+		}
 
-    return msg;
-  }
+		return msg;
+	}
 
-  /**
-   * Returns the same as {@link #checkChar(char c) checkChar(char c)}, but
-   * formatted as HTML. Checks whether an entered letter is part of the word
-   * or not or has already been guessed.
-   * 
-   * @param c
-   *          The character to be checked.
-   * @return A short evaluating message formatted as HTML.
-   */
-  public String checkCharHtml(char c) {
-    String answer = checkChar(c);
-    String answerHtml = "<P>" + answer.replace("\r\n", "<BR>") + "</P>";
-    return answerHtml;
-  }
+	/**
+	 * Returns the same as {@link #checkChar(char c) checkChar(char c)}, but
+	 * formatted as HTML. Checks whether an entered letter is part of the word
+	 * or not or has already been guessed.
+	 * 
+	 * @param c
+	 *          The character to be checked.
+	 * @return A short evaluating message formatted as HTML.
+	 */
+	public String checkCharHtml(char c) {
+		String answer = checkChar(c);
+		String answerHtml = "<P>" + answer.replace("\r\n", "<BR>") + "</P>";
+		return answerHtml;
+	}
 
-  /**
-   * Checks whether the entered word is the correct solution.
-   * 
-   * @param input
-   *              The word to be checked.
-   * @return A short evaluation message.
-   */
-  public String checkWord(String input) {
-    String msg;
+	/**
+	 * Checks whether the entered word is the correct solution.
+	 * 
+	 * @param input
+	 *              The word to be checked.
+	 * @return A short evaluation message.
+	 */
+	public String checkWord(String input) {
+		String msg;
 
-    if (input.compareToIgnoreCase(word) == 0) {
-      solved = true;
-      msg = "Absolut richtig!\r\n";
-    } else {
-      ++numMissed;
-      msg = "Leider falsch!\r\n";
-    }
+		if (input.compareToIgnoreCase(word) == 0) {
+			solved = true;
+			msg = "Absolut richtig!\r\n";
+		} else {
+			++numMissed;
+			msg = "Leider falsch!\r\n";
+		}
 
-    return msg;
-  }
+		return msg;
+	}
 
-  /**
-   * Returns the same as {@link #checkWord(String input) checkWord(String
-   * input)}, but formatted as HTML. Checks whether the entered word is the
-   * correct solution.
-   * 
-   * @param input
-   *              The word to be checked.
-   * @return A short evaluating message formatted as HTML.
-   */
-  public String checkWordHtml(String input) {
-    String answer = checkWord(input);
-    String answerHtml = "<P>" + answer.replace("\r\n", "<BR>") + "</P>";
-    return answerHtml;
-  }
+	/**
+	 * Returns the same as {@link #checkWord(String input) checkWord(String
+	 * input)}, but formatted as HTML. Checks whether the entered word is the
+	 * correct solution.
+	 * 
+	 * @param input
+	 *              The word to be checked.
+	 * @return A short evaluating message formatted as HTML.
+	 */
+	public String checkWordHtml(String input) {
+		String answer = checkWord(input);
+		String answerHtml = "<P>" + answer.replace("\r\n", "<BR>") + "</P>";
+		return answerHtml;
+	}
 
-  /**
-   * Returns the word to be guessed.
-   * 
-   * @return The word to be guessed.
-   */
-  public String getWord() {
-    return word;
-  }
+	/**
+	 * Returns the word to be guessed.
+	 * 
+	 * @return The word to be guessed.
+	 */
+	public String getWord() {
+		return word;
+	}
 
-  /**
-   * Returns the current state of hangman including the found and missed
-   * letters.
-   * 
-   * @return The current state of hangman.
-   */
-  public String getHangman() {
-    StringBuilder hangman = new StringBuilder(128);
+	/**
+	 * Returns the current state of hangman including the found and missed
+	 * letters.
+	 * 
+	 * @return The current state of hangman.
+	 */
+	public String getHangman() {
+		StringBuilder hangman = new StringBuilder(128);
 
-    if (numMissed < 2) {
-      hangman.append("|       \r\n");
-    } else {
-      hangman.append("|  +==+  \r\n");
-    }
+		if (numMissed < 2) {
+			hangman.append("|       \r\n");
+		} else {
+			hangman.append("|  +==+  \r\n");
+		}
 
-    if (numMissed < 3) {
-      hangman.append("|     |\r\n");
-    } else {
-      hangman.append("|  |  |\r\n");
-    }
+		if (numMissed < 3) {
+			hangman.append("|     |\r\n");
+		} else {
+			hangman.append("|  |  |\r\n");
+		}
 
-    if (numMissed < 4) {
-      hangman.append("|     |\r\n");
-    } else {
-      hangman.append("|  o  |\r\n");
-    }
+		if (numMissed < 4) {
+			hangman.append("|     |\r\n");
+		} else {
+			hangman.append("|  o  |\r\n");
+		}
 
-    if (numMissed < 5) {
-      hangman.append("|     |");
-    } else if (numMissed < 6) {
-      hangman.append("|  |  |");
-    } else if (numMissed < 7) {
-      hangman.append("| /|  |");
-    } else {
-      hangman.append("| /|\\ |");
-    }
+		if (numMissed < 5) {
+			hangman.append("|     |");
+		} else if (numMissed < 6) {
+			hangman.append("|  |  |");
+		} else if (numMissed < 7) {
+			hangman.append("| /|  |");
+		} else {
+			hangman.append("| /|\\ |");
+		}
 
-    hangman.append("\r\n");
+		hangman.append("\r\n");
 
-    if (numMissed < 8) {
-      hangman.append("|     |");
-    } else if (numMissed < 9) {
-      hangman.append("| /   |");
-    } else {
-      hangman.append("| / \\ |");
-    }
+		if (numMissed < 8) {
+			hangman.append("|     |");
+		} else if (numMissed < 9) {
+			hangman.append("| /   |");
+		} else {
+			hangman.append("| / \\ |");
+		}
 
-    hangman.append("  Nicht enthalten: ");
-    boolean first = true;
-    for (Character s : misses) {
-      if (first) {
-        hangman.append(s);
-        first = false;
-      } else {
-        hangman.append(", ");
-        hangman.append(s);
-      }
-    }
-    hangman.append("\r\n");
-    hangman.append("|_____|  ");
-    hangman.append("\r\n\r\n");
+		hangman.append("  Nicht enthalten: ");
+		boolean first = true;
+		for (Character s : misses) {
+			if (first) {
+				hangman.append(s);
+				first = false;
+			} else {
+				hangman.append(", ");
+				hangman.append(s);
+			}
+		}
+		hangman.append("\r\n");
+		hangman.append("|_____|  ");
+		hangman.append("\r\n\r\n");
 
-    if (solved) {
-      hangman.append(word.toLowerCase());
-    } else {
-      for (int i = 0; i < word.length(); ++i) {
-        if (guesses.contains(word.toLowerCase().charAt(i))) {
-          hangman.append(word.toLowerCase().charAt(i));
-        } else {
-          hangman.append("*");
-        }
-      }
-    }
+		if (solved) {
+			hangman.append(word.toLowerCase());
+		} else {
+			for (int i = 0; i < word.length(); ++i) {
+				if (guesses.contains(word.toLowerCase().charAt(i))) {
+					hangman.append(word.toLowerCase().charAt(i));
+				} else {
+					hangman.append("*");
+				}
+			}
+		}
 
-    return hangman.toString();
-  }
+		return hangman.toString();
+	}
 
-  /**
-   * Returns the same as {@link #getHangman() getHangman()}, but formatted as
-   * HTML. Returns the current state of hangman including the found and missed
-   * letters.
-   * 
-   * @return The current state of hangman formatted as HTML.
-   */
-  public String getHangmanHtml() {
-    String hm = getHangman();
-    String hmHtml = "<PRE>" + hm.replace("\r\n", "<BR>") + "</PRE><BR>";
-    return hmHtml;
-  }
+	/**
+	 * Returns the same as {@link #getHangman() getHangman()}, but formatted as
+	 * HTML. Returns the current state of hangman including the found and missed
+	 * letters.
+	 * 
+	 * @return The current state of hangman formatted as HTML.
+	 */
+	public String getHangmanHtml() {
+		String hm = getHangman();
+		String hmHtml = "<PRE>" + hm.replace("\r\n", "<BR>") + "</PRE><BR>";
+		return hmHtml;
+	}
 
-  /**
-   * Checks whether the word has been found.
-   * 
-   * @return True if the word has been found, false otherwise.
-   */
-  public boolean win() {
-    if (solved)
-      return true;
-    for (int i = 0; i < word.length(); ++i) {
-      if (!guesses.contains(word.toLowerCase().charAt(i))) {
-        return false;
-      }
-    }
-    return true;
-  }
+	/**
+	 * Checks whether the word has been found.
+	 * 
+	 * @return True if the word has been found, false otherwise.
+	 */
+	public boolean win() {
+		if (solved)
+			return true;
+		for (int i = 0; i < word.length(); ++i) {
+			if (!guesses.contains(word.toLowerCase().charAt(i))) {
+				return false;
+			}
+		}
+		return true;
+	}
 
-  /**
-   * Checks if the hangman is dead due to too many wrong guesses.
-   * 
-   * @return True if the hangman is dead, false otherwise.
-   */
-  public boolean dead() {
-    if (numMissed >= 9)
-      return true;
-    else
-      return false;
-  }
+	/**
+	 * Checks if the hangman is dead due to too many wrong guesses.
+	 * 
+	 * @return True if the hangman is dead, false otherwise.
+	 */
+	public boolean dead() {
+		if (numMissed >= 9)
+			return true;
+		else
+			return false;
+	}
 
 }
--- Server.java
+++ Server.java
@@ -12,144 +12,148 @@ import java.net.Socket;
  * methods and fields are static.
  */
 public class Server {
-  // ServerSocket
-  private static ServerSocket serverSocket;
+	private static final int portnumber = 2345;
+	private static ServerSocket serverSocket;
 
-  // Client Socket array
-  private static Socket[] clientSockets;
+	/** Active connections to players */
+	private static Socket[] connections;
 
-  /** A BufferedWriter for each player. */
-  private static BufferedWriter[] writers;
+	/** A BufferedWriter for each player. */
+	private static BufferedWriter[] writers;
 
-  /** A BufferedReader for each player; */
-  private static BufferedReader[] readers;
+	/** A BufferedReader for each player; */
+	private static BufferedReader[] readers;
 
-  /** Number of players. */
-  private static final int NUM_PLAYERS = 2;
+	/** Number of players. */
+	private static final int NUM_PLAYERS = 2;
 
-  /** Currently active player (0 - n-1). */
-  private static int curPlayer;
-
-  private static final int portnumber = 2345;
-  /**
-   * Initializes the game. Loops until solution is found or hangman is dead.
-   * 
-   * @param argv
-   *             Optional command line arguments.
-   * @throws Exception
-   */
-  public static void main(String[] argv) throws Exception {
-    //initialise game and Hangman
-    initGame();
-    Hangman hangman = new Hangman();
-
-    while (!hangman.win() && !hangman.dead())// Loop until solution is found or hangman is dead.
-    {
-      // Inform players and read input.
-        writers[curPlayer].write("\n its your turn to guess \n");
-        writers[curPlayer].flush();
-
-        //Process input
-        String output;
-        String input = readers[curPlayer].readLine();
-        if(input.length() > 1){
-          output = hangman.checkWord(input);
-        }else if(input.length() == 1){
-          output = hangman.checkChar(input.charAt(0));
-        }else{
-          output = "No character/word entered";
-          writers[curPlayer].write("Please enter a character\n");
-          writers[curPlayer].flush();
-        }
-      writeToAllButCur("Player " + (curPlayer + 1) + " guessed " + input
-              + "\n " + output);
-
-      //  Process input and inform players.
-      writeToAll(hangman.getHangman());
-      //  Set curPlayer to next player.
-      curPlayer = (curPlayer + 1) % NUM_PLAYERS;
-    }
-
-    // Inform players about the game result.
-    if(hangman.dead()){
-      writeToAll("You lost :(");
-    } else if (hangman.win()) {
-      writeToAll("You won!!!");
-    }
-    // Close player sockets.
-    for (Socket socket : clientSockets){
-      socket.close();
-    }
-  }
-
-  /**
-   * Initializes sockets until number of players {@link #NUM_PLAYERS
-   * NUM_PLAYERS} is reached.
-   * 
-   * @throws Exception
-   */
-  private static void initGame() throws Exception {
-
-    serverSocket = new ServerSocket(portnumber);
-    clientSockets = new Socket[NUM_PLAYERS];
-
-    writers = new BufferedWriter[NUM_PLAYERS];
-    readers = new BufferedReader[NUM_PLAYERS];
-
-    curPlayer = 0;
-
-    System.out.println("Server is running on Port " + portnumber + ", waiting for players...");
+	/** Currently active player (0 - n-1). */
+	private static int curPlayer;
 
 
-    while (curPlayer < NUM_PLAYERS)
-    {
-      Socket clientSocket = serverSocket.accept();
-      clientSockets[curPlayer] = clientSocket;
+	/**
+	 * Initializes the game. Loops until solution is found or hangman is dead.
+	 * 
+	 * @param argv Optional command line arguments.
+	 * @throws Exception
+	 */
+	public static void main(String[] argv) throws Exception {
+		// Initialise game and Hangman
+		initGame();
+		Hangman hangman = new Hangman();
 
-      writers[curPlayer] = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
-      readers[curPlayer] = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		// Loop until solution is found or hangman is dead.
+		while (!hangman.win() && !hangman.dead()) {
+			// Inform players of their turn
+			writers[curPlayer].write("\nIts your turn to guess\n");
+			writers[curPlayer].flush();
+			//TODO: Skip values typed while the other players were guessing
 
-      writers[curPlayer].write("Connection successful, welcome to the Game\n ");
-      writers[curPlayer].flush();
+			// Process input
+			String output;
+			String input;
+			while (true) {
+				writers[curPlayer].write("Your guess: ");
+				writers[curPlayer].flush();
+				input = readers[curPlayer].readLine();
+				if (input.length() > 0 && input.charAt(0) == '!') {
+					output = hangman.checkWord(input.substring(1));
+					break;
+				} else if (input.length() == 1) {
+					output = hangman.checkChar(input.charAt(0));
+					break;
+				} else {
+					output = "No character/word entered";
+					writers[curPlayer].write("Please enter a character, or !<word> to guess the full word\n");
+					writers[curPlayer].flush();
+				}
+			}
+			writeToAllButCur("Player " + (curPlayer + 1) + " guessed " + input + "\n");
+			writeToAll(output);
+			writeToAll(hangman.getHangman() + "\n\n");
 
-      curPlayer++;
-    }
-    // Reset current player.
-    writeToAll("Game starting\n ");
-    curPlayer = 0;
+			// Set curPlayer to next player.
+			curPlayer = (curPlayer + 1) % NUM_PLAYERS;
+		}
 
-    // Prevent more connections to be established. Inform players about start of the game.
+		// Inform players about the game result.
+		if (hangman.dead()) {
+			writeToAll("You lost :(\n");
+		} else if (hangman.win()) {
+			writeToAll("You won!!!\n");
+		}
 
-  }
+		// Close connections
+		for (Socket socket : connections) {
+			socket.close();
+		}
+		serverSocket.close();
+	}
 
-  /**
-   * Writes the String s to all players.
-   * @param s // The String to be sent.
-   * @throws Exception
-   */
-  private static void writeToAll(String s) throws Exception {
-    for (int i = 0; i < NUM_PLAYERS; i++){
-      BufferedWriter writer = writers[i];
-      writer.write(s);
-      writer.flush();
-    }
-  }
+	/**
+	 * Initializes sockets until number of players {@link #NUM_PLAYERS
+	 * NUM_PLAYERS} is reached.
+	 * 
+	 * @throws Exception
+	 */
+	private static void initGame() throws Exception {
+		serverSocket = new ServerSocket(portnumber);
+		connections = new Socket[NUM_PLAYERS];
 
-  /**
-   * Writes the String s to all players but to the current player.
-   * 
-   * @param s //The String to be sent.
-   * @throws Exception
-   */
-  private static void writeToAllButCur(String s) throws Exception {
-    for (int i = 0; i < NUM_PLAYERS; i++){
-      if(i == curPlayer){
-        continue;
-      }
-      BufferedWriter writer = writers[i];
-      writer.write(s);
-      writer.flush();
-    }
-  }
+		writers = new BufferedWriter[NUM_PLAYERS];
+		readers = new BufferedReader[NUM_PLAYERS];
 
+		System.out.println("Server is running on Port " + portnumber + ", waiting for " + NUM_PLAYERS + " players...");
+
+		for (int i = 0; i < NUM_PLAYERS; i++) {
+			Socket newConnection = serverSocket.accept();
+			connections[i] = newConnection;
+
+			System.out.println("New player connected from " + newConnection.getInetAddress().getHostAddress() + " (" + (i+1) + "/" + NUM_PLAYERS + ")");
+			writeToAll("Player connected (" + (i+1) + "/" + NUM_PLAYERS + ")\n");
+
+			writers[i] = new BufferedWriter(new OutputStreamWriter(newConnection.getOutputStream()));
+			readers[i] = new BufferedReader(new InputStreamReader(newConnection.getInputStream()));
+
+			writers[i].write("Connection successful, welcome to the Game\n");
+			writers[i].write("" + (i+1) + "/" + NUM_PLAYERS + " players found\n");
+			writers[i].flush();
+		}
+		
+		// Prevent more connections to be established. Inform players about start of the
+		// game.
+		writeToAll("Game starting\n");
+	}
+
+	/**
+	 * Writes the String s to all players.
+	 * 
+	 * @param s The String to be sent.
+	 * @throws Exception
+	 */
+	private static void writeToAll(String s) throws Exception {
+		for (int i = 0; i < NUM_PLAYERS; i++) {
+			BufferedWriter writer = writers[i];
+			if (writer == null) continue; // Safe access during init
+			writer.write(s);
+			writer.flush();
+		}
+	}
+
+	/**
+	 * Writes the String s to all players but to the current player.
+	 * 
+	 * @param s The String to be sent.
+	 * @throws Exception
+	 */
+	private static void writeToAllButCur(String s) throws Exception {
+		for (int i = 0; i < NUM_PLAYERS; i++) {
+			BufferedWriter writer = writers[i];
+			if (i == curPlayer || writer == null) { // Safe access during init (unused)
+				continue;
+			}
+			writer.write(s);
+			writer.flush();
+		}
+	}
 }
